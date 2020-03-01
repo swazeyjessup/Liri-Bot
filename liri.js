cff81cf01c08b6f58af71d9a9edb8dc33671f19d
Swazey Jessup <swazey_jessup@hotmail.com>
2020-03-01 12:13:08 -0700

    added spotifiy links

--- liri.js
+++ liri.js
@@ -1,5 +1,10 @@
 require("dotenv").config();
 
 var keys = require("./keys.js");
-
+var axios = require("axios");
+var moment = require("moment");
+var Spotify = require('node-spotify-api');
+var fs = require("fs");
 var spotify = new Spotify(keys.spotify);
+
+
