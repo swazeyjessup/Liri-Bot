1d9eb25b3606ceeb146f1acc379b17686685d21c
Swazey Jessup <swazey_jessup@hotmail.com>
2020-03-01 13:56:41 -0700

    Added concert this and function

--- liri.js
+++ liri.js
@@ -8,3 +8,47 @@ var fs = require("fs");
 var spotify = new Spotify(keys.spotify);
 
 
+//default movie //
+var defaultMovie = "Mr. Nobody";
+var defaultSong = "I Saw the Sign"
+
+var action = process.argv[2];
+var value = process.argv[3];
+
+switch (action) {
+    case "concert-this":
+        getBands(value)
+        break;
+    case "spotify-this-song":
+            if (value === ""){
+                value = defaultSong;
+            }
+        getSongs(value)
+        break;
+    case "movie-this":
+        if (value === ""){
+            value = defaultMovie;
+        }
+        getMovies(value)
+        break;
+    case "do-what-it-says":
+        doWhatItSays()
+        break;
+    default:
+        break;
+}
+
+function getBands(artist) {
+    axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
+        .then(function(response){
+            console.log("Name of the venue:", response.data[0].venue.name);
+            console.log("Venue location:", response.data[0].venue.city);
+            var eventDate = moment(response.data[0].datetime).format('MM/DD/YYYY');
+            console.log("Date of the Event:", eventDate);
+        })
+        .catch(function (error){
+            console.log(error);
+        });
+}
+
+ 
